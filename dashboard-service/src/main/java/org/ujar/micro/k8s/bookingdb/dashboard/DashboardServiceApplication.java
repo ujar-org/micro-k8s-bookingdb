313b6e65b2a99dfa6c28cd7a46b1f8aafed1493f
Dima Denysenko <coding@dimdnk.com>
2022-07-07 09:56:28 +0300

    feat: use dashboard-service naming

---
+++ dashboard-service/src/main/java/org/ujar/micro/k8s/bookingdb/dashboard/DashboardServiceApplication.java
@@ -0,0 +1,11 @@
+package org.ujar.micro.k8s.bookingdb.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DashboardServiceApplication.class, args);
+  }
+}
---
+++ dashboard-service/src/main/java/org/ujar/micro/k8s/bookingdb/dashboard/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package org.ujar.micro.k8s.bookingdb.dashboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.ujar.boot.starter.logbook.LogbookJsonBodyFilter;
+import org.ujar.boot.starter.logbook.LogbookResponseOnStatus;
+import org.ujar.micro.k8s.bookingdb.jobs.annotation.EnableJobSupport;
+import org.ujar.micro.k8s.bookingdb.persistence.annotation.EnableBookingPersistence;
+
+@Configuration
+@LogbookResponseOnStatus
+@LogbookJsonBodyFilter
+@EnableJobSupport
+@EnableBookingPersistence
+public class ApplicationConfig {
+}
