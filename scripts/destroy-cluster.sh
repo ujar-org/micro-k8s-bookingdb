087c5c16e1df2fc2f97df8e08815a988d5876d79
Dima Denysenko <coding@dimdnk.com>
2022-07-14 19:37:51 +0300

    feat: configure ingress

--- scripts/destroy-cluster.sh
+++ scripts/destroy-cluster.sh
@@ -9,16 +9,10 @@ set -x
 kubectl config set-context $CLUSTER_NAME
 kubectl config use-context $CLUSTER_NAME
 
-kubectl delete clusterrolebinding service-pod-reader-init-container
-kubectl delete clusterrolebinding service-pod-reader-dashboard
-kubectl delete clusterrolebinding service-pod-reader-importer
-kubectl delete clusterrolebinding service-pod-reader-rabbitmq
-kubectl delete clusterrolebinding service-pod-reader-mysql
+kubectl delete clusterrolebinding service-pod-reader-$K8S_NAMESPACE
 
 kubectl delete serviceaccount $SA_NAME -n $K8S_NAMESPACE
 
 kubectl delete clusterrole microservices-kubernetes-namespace-reader
 
 kubectl delete namespace $K8S_NAMESPACE
-
-exit;
