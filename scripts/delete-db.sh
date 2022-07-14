5edf8596b2b4617af38a529b4b8ce2aeda6cfee9
Dima Denysenko <coding@dimdnk.com>
2022-07-14 13:20:13 +0300

    feat: configure k8s deployments

---
+++ scripts/delete-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+.  ./set-env.sh
+
+kubectl config set-context $CLUSTER_NAME
+kubectl config use-context $CLUSTER_NAME
+
+kubectl delete -n $K8S_NAMESPACE deployment mysql
+kubectl delete -n $K8S_NAMESPACE service mysql
+kubectl delete -n $K8S_NAMESPACE configmap mysql
+kubectl delete -n $K8S_NAMESPACE secret mysql
