3dc9229d239d6668fd2fdda5f9f1be0d86652330
Dima Denysenko <coding@dimdnk.com>
2022-07-16 12:18:57 +0300

    feat: change configmap

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -5,7 +5,7 @@ set -x
 
 . ./set-env.sh
 
-minikube start -p $CLUSTER_NAME --memory='4000mb' --cpus=2 --disk-size=40g --insecure-registry=localhost:5000
+minikube start -p $CLUSTER_NAME --memory='4000mb' --cpus=4 --disk-size=25g --insecure-registry=localhost:5000
 minikube profile $CLUSTER_NAME
 
 minikube addons enable metrics-server
