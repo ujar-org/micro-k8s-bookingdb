b182f64e964b028180a522a5e3308e703ad73355
Dima Denysenko <coding@dimdnk.com>
2022-07-19 11:53:06 +0300

    chore: increase mem limit for minikube

--- scripts/install-infra.sh
+++ scripts/install-infra.sh
@@ -11,11 +11,11 @@ kubectl config use-context $CLUSTER_NAME
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install -n $K8S_NAMESPACE prometheus prometheus-community/prometheus
-kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np
+kubectl expose service prometheus-server -n $K8S_NAMESPACE --type=NodePort --target-port=9090 --name=prometheus-server-np
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm install -n $K8S_NAMESPACE grafana stable/grafana
-kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-np
+kubectl expose service grafana -n $K8S_NAMESPACE --type=NodePort --target-port=3000 --name=grafana-np
 kubectl get secret -n $K8S_NAMESPACE grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
 kubectl apply -n $K8S_NAMESPACE -f mysql/configmap.yaml
