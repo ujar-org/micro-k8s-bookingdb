4d53a9f5282f05d967b2ee2b32934f2638e42ca8
Dima Denysenko <coding@dimdnk.com>
2022-07-27 19:28:19 +0300

    feat: made graphql queries parameterized by page & size

--- edge-service/src/main/java/org/ujar/micro/k8s/bookingdb/edge/service/DashboardService.java
+++ edge-service/src/main/java/org/ujar/micro/k8s/bookingdb/edge/service/DashboardService.java
@@ -1,6 +1,5 @@
 package org.ujar.micro.k8s.bookingdb.edge.service;
 
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
